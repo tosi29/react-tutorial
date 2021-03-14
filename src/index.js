4ef3bcbe57c5030cd064f24f51ad10a935edee9d
tosi29 <tosi16@gmail.com>
2021-03-14 17:06:12 +0900

    Implement Draw game

--- src/index.js
+++ src/index.js
@@ -123,7 +123,10 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
-    } else {
+    } else if (this.state.stepNumber == 9) {
+      status = 'Draw...'
+    }
+    else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
 
